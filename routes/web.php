2b691c62b3e443cec74bf1ec603331f884a8d9e9
Saugat Chaudhary <saugat.chaudhary@volgai.com>
2025-11-14 16:53:03 +0545

    removed test code from route file

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $ticket = \App\Models\Ticket::find(2);
-    dd($ticket->bookings()->where('status', '!=', 'CANCELLED')->sum('quantity'));
     return view('welcome');
 });
